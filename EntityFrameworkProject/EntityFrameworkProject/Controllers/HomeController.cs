a9a895b7a27047cad5b09ee28a807a967fbc3753
Cavid Bashirov <bashirovcavid93gmail.com>
2022-10-05 11:29:45 +0400

    initial

---
+++ EntityFrameworkProject/EntityFrameworkProject/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using EntityFrameworkProject.Data;
+using EntityFrameworkProject.Models;
+using EntityFrameworkProject.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AppDbContext _context;
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+
+            List<Slider> sliders = _context.Sliders.ToList();
+            SliderDetail sliderDetail = _context.SliderDetails.FirstOrDefault();
+
+            HomeVM model = new HomeVM
+            {
+                Sliders = sliders,
+                SliderDetail = sliderDetail
+            };
+
+            return View(model);
+        }
+
+    }
+}
---
+++ EntityFrameworkProject/EntityFrameworkProject/ViewModels/HomeVM.cs
@@ -0,0 +1,11 @@
+﻿using EntityFrameworkProject.Models;
+using System.Collections.Generic;
+
+namespace EntityFrameworkProject.ViewModels
+{
+    public class HomeVM
+    {
+        public List<Slider> Sliders { get; set; }
+        public SliderDetail SliderDetail { get; set; }
+    }
+}
